f34ab34af8a0b7cca91700c8a287bbe7f7808d28
bgarciabtk <bgarcia@btkfarma.com>
2025-03-27 21:56:24 -0600

    add flip card

--- src/Components/atoms/Card/index.jsx
+++ src/Components/atoms/Card/index.jsx
@@ -1,12 +1,19 @@
 import { Col } from "react-bootstrap";
-import "./index.css"
+import "./index.css";
 
 export const Card = (props) => {
   return (
-    <Col md={2} xs={2} lg={2}>
-        <img src={props?.url} alt={props.title} className="image-card " />
-    </Col>
-  )
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front bg-x3dark">
+        </div>
+        <div className="flip-card-back">
+         <img src={props?.url} alt={props.title} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
+
--- src/Components/molecules/Dashboard/index.jsx
+++ src/Components/molecules/Dashboard/index.jsx
@@ -15,10 +15,10 @@ export const DashboardGame = () => {
   console.log(cards);
   return (
     <>
-      <Container fluid={"sm md lg lx xxl"}>
-        <Row bsPrefix="row" xxl={12} xl={12} lg={12} md={12} sm={12} xs={12} style={{ flexWrap: "wrap"}}> 
-          {cards?.map((item) => (
-            <Card {...item} />
+      <Container fluid={"sm md lg lx xxl"} className="bg-x2dark">
+        <Row bsPrefix="row" xxl={12} xl={12} lg={12} md={12} sm={12} xs={12} style={{ display: "flex",  flexWrap: "wrap"}}> 
+          {cards?.map((item, index) => (
+            <Card key={index} {...item} />
           ))}
         </Row>
       </Container>
