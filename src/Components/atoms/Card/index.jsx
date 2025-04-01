1d91bf9a7e02d1509ec99451b2c449879905cfc3
bgarciabtk <bgarcia@btkfarma.com>
2025-04-01 17:45:00 -0600

    set high priority

--- src/Components/atoms/Card/index.jsx
+++ src/Components/atoms/Card/index.jsx
@@ -7,7 +7,7 @@ export const Card = ({ url, title, displayCard, matched }) => {
       <div className={`flip-card-inner  ${matched ? "display-card" : ""}`}>
         <div className="flip-card-front bg-x3dark"></div>
         <div className="flip-card-back">
-          <img src={url} alt={title} loading="lazy" />
+          <img src={url} alt={title} loading="lazy" fetchpriority="high"/>
         </div>
       </div>
     </div>
